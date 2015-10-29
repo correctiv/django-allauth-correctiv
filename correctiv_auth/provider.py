1899fd62384c44c56e844d83de4d5ac04df869e1
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2015-10-29 17:01:33 +0100

    Make email truly optional, use id for identification

--- correctiv_auth/provider.py
+++ correctiv_auth/provider.py
@@ -1,6 +1,10 @@
+from allauth.account.models import EmailAddress
+
 from allauth.socialaccount import providers
 from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
+
+from allauth.socialaccount import app_settings
 
 
 class CorrectivAccount(ProviderAccount):
@@ -19,13 +23,30 @@ class CorrectivProvider(OAuth2Provider):
     package = 'correctiv_auth'
     account_class = CorrectivAccount
 
+    def get_default_scope(self):
+        scope = ['read:profile']
+        if app_settings.QUERY_EMAIL:
+            scope.append('read:email')
+        return scope
+
     def extract_uid(self, data):
-        return str(data['email'])
+        return str(data['id'])
+
+    def extract_email_addresses(self, data):
+        if 'email' in data:
+            return [EmailAddress(email=data.get('email').lower(),
+                          verified=True,
+                          primary=True)]
+        return []
 
     def extract_common_fields(self, data):
-        return dict(email=data.get('email'),
-                    last_name=data.get('last_name'),
-                    first_name=data.get('first_name'))
+        fields = {
+            'last_name': data.get('last_name'),
+            'first_name': data.get('first_name'),
+        }
+        if 'email' in data:
+            fields['email'] = data.get('email')
+        return fields
 
 
 providers.registry.register(CorrectivProvider)
